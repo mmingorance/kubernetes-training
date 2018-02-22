7e1255982cb8a29c27f77c647c7ade4f62bed573
Miguel Mingorance <m.mingorance@foodpanda.com>
2018-02-22 14:28:17 +0100

    new AZ added to cluster

--- setup-kubernetes-vars.sh
+++ setup-kubernetes-vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CLUSTER_NAME="eu.mingorance-k8s.deliveryhero.net"
-export REGION_ZONES="eu-central-1a"
+export REGION_ZONES="eu-central-1a,eu-central-1b,eu-central-1c"
 export NUMBER_OF_NODES=3
 export SIZE_OF_NODES="t2.micro"
 export NUMBER_OF_MASTERS=1
