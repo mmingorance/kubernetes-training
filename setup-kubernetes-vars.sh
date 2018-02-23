7e7979f02c7684ad673b16f845931eedd907865a
Miguel Mingorance <m.mingorance@foodpanda.com>
2018-02-23 14:04:01 +0100

    Added new parameters for cluster

--- setup-kubernetes-vars.sh
+++ setup-kubernetes-vars.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export CLUSTER_NAME="eu.mingorance-k8s.deliveryhero.net"
+export SSH_PUBLIC_KEY="~/.ssh/dh_rsa.pub"
 export REGION_ZONES="eu-central-1a,eu-central-1b,eu-central-1c"
 export NUMBER_OF_NODES=3
 export SIZE_OF_NODES="t2.micro"
--- setup-kubernetes.sh
+++ setup-kubernetes.sh
@@ -2,9 +2,11 @@
 source ~/Kubernetes-Training/setup-kubernetes-vars.sh
 kops create cluster \
 --name ${CLUSTER_NAME} \
+--ssh-public-key ${SSH_PUBLIC_KEY} \
 --zones ${REGION_ZONES} \
 --node-count ${NUMBER_OF_NODES} \
 --node-size ${SIZE_OF_NODES} \
 --master-count ${NUMBER_OF_MASTERS} \
 --master-size ${SIZE_OF_MASTERS} \
+--topology public \
 --yes
